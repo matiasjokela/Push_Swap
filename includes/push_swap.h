071f9edf4a422e2c5ffb45ae7d6ffca86a507a78
Matias Jokela <mjokela@c2r4p7.hive.fi>
2022-06-15 18:41:35 +0300

    Added header and struct for stacks

---
+++ includes/push_swap.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjokela <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/15 18:07:25 by mjokela           #+#    #+#             */
+/*   Updated: 2022/06/15 18:08:04 by mjokela          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include ".././libft/libft.h"
+# include <stdio.h>															//REMOVE
+
+
+
+typedef struct s_stack
+{
+	int				value;
+	struct s_stack	*next;
+}					t_stack;
+
+
+#endif
